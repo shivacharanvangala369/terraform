3ce143d1080c4973978110e8b89669ced354bf9c
shivacharanvangala369 <shivacharan.vangala369@gmail.com>
2026-03-13 13:29:38 +0530

    terraform

--- count/rot53.tf
+++ count/rot53.tf
@@ -7,3 +7,17 @@ count = 10
   ttl     = 1
   records = [aws_instance.ec2[count.index].private_ip]
 }
+
+
+# record for roboshop.devops.online
+
+resource "aws_route53_record" "www" {
+
+  zone_id = var.zone_id
+  # inetrpolation  = mixing a varialble and extra char
+  name    =  "roboshop.${var.domain_name}"  # roboshop.devocops.online
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.ec2[index(var.instances, "frontend")].public_ip]
+}
+
--- for_each/rot53.tf
+++ for_each/rot53.tf
@@ -8,3 +8,18 @@ resource "aws_route53_record" "www" {
   records = [each.value.private_ip]
   allow_overwrite = true
 }
+
+
+
+# record for roboshop.devops.online
+
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.ec2
+  zone_id = var.zone_id
+  # inetrpolation  = mixing a varialble and extra char
+  name    =  "roboshop.${var.domain_name}"  # roboshop.devocops.online
+  type    = "A"
+  ttl     = 1
+  records = [lookup(aws_instance.ec2, "frontend").public_ip]
+  allow_overwrite = true
+}
